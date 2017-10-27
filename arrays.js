f89b579afaa8b660fbeccc2f3acd2cf0f527b2fa
Abigail Messmer <abbie.messmer@gmail.com>
2017-10-27 18:54:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,34 @@
 var chocolateBars = [ 'snickers','hundred grand','kitkat','skittles']
+<<<<<<< HEAD
 //console.log(chocolateBars)
 
 //const array = [1]
 //const element = 'foo'
+=======
+console.log(chocolateBars)
+
+const array = [1]
+const element = 'foo'
+>>>>>>> df2e2ab46fe028b565d23895346a314dbb867e43
 
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+<<<<<<< HEAD
 //console.log(array)
+=======
+console.log(array)
+>>>>>>> df2e2ab46fe028b565d23895346a314dbb867e43
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
       array.unshift(element);
   return array
 }
+<<<<<<< HEAD
 //console.log(destructivelyAddElementToBeginningOfArray(array))
+=======
+console.log(destructivelyAddElementToBeginningOfArray(array))
+>>>>>>> df2e2ab46fe028b565d23895346a314dbb867e43
 
 function addElementToEndOfArray(array,element) {
   return [...array, element]
@@ -27,6 +42,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array,index) {
   return array[2]
 }
+<<<<<<< HEAD
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
       array.shift()
@@ -51,3 +67,5 @@ function removeElementFromEndOfArray(array){
   array = array.slice(0, array.length - 1 )
   return array
 }
+=======
+>>>>>>> df2e2ab46fe028b565d23895346a314dbb867e43
